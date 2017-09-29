39c8a3a01fc525b1c68039b3ff6a0271ace0fce6
rahul-aravinth <32102821+rahul-aravinth@users.noreply.github.com>
2017-09-29 21:41:50 +0530

    Update Star.java

--- Star.java
+++ Star.java
@@ -3,9 +3,9 @@ public class Star {
 	public static void main(String args[]){
 		for(int i=1;i<=5;i++)
     {
-			for(int j=1;j<=5;j++)
+			for(int j=1;j<=i;j++)
       {
-				System.out.print("*");
+				System.out.print("*");	
 			}
 			System.out.println();
 		}
