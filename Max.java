746f22ad817ed435c41b65b6495295af9d79c2d1
rahul-aravinth <32102821+rahul-aravinth@users.noreply.github.com>
2017-09-29 22:10:52 +0530

    Update Max.java

--- Max.java
+++ Max.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class Max {
 	public static void main(String args[])throws IOException{
 		Scanner sc=new Scanner(System.in);
-		int ary[]=new int[5];
+		int ary[]=new int[25];
 		int max=0;
 		try{
-		for(int i=1;i<=5;i++){
+		for(int i=1;i<=25;i++){
 			System.out.println("Enter number for element ary["+i+"]:");
 			ary[i]=sc.nextInt();
 			max=(ary[i]>max?max=ary[i]:max);
