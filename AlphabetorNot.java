10a4d05f31663899dce15fbb89aa3362b0d31baa
rahul-aravinth <32102821+rahul-aravinth@users.noreply.github.com>
2018-01-03 21:48:43 +0530

    Create AlphabetorNot.java

---
+++ AlphabetorNot.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class AlphabetorNot
+{
+public static void main(String s[])
+{
+char ch;
+Scanner sc=new Scanner(System.in)
+ch=sc.next();
+if(ch>='A'&&ch<='Z'||ch>='a'&&ch<='z')
+System.out.println("alphabet");
+}
+}
