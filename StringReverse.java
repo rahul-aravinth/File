e132db2e37824466e841bc0d0ce5b2ab2061c51a
Rahul <32102821+rahul-aravinth@users.noreply.github.com>
2018-01-23 19:41:01 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,7 @@
+public class srev {
+   public static void main(String[] args){
+	   String s1="karthik";
+	   String s2=new StringBuffer(s1).reverse().toString();
+	   System.out.println(s2);
+   }
+}
